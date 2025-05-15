dd1986e45695779c049d72847f29780efcd0e9a4
MurrayJaneMary <101001192+MurrayJaneMary@users.noreply.github.com>
2025-05-15 13:24:02 +0100

    update model, create backup copy

---
+++ addToPath.m
@@ -0,0 +1,1 @@
+addpath(genpath('C:/Users/murray98/Documents/Bruce Buffett model/code/Subscripts_and_functions'))
--- read_in_combined_outputs.m
+++ read_in_combined_outputs.m
@@ -1,13 +1,13 @@
 % Define the path to the large CSV file
-filePath = 'C:\Users\murray98\Documents\Bruce Buffett model\code\model2\combined_output.csv';
+filePath = 'C:\Users\murray98\Documents\Bruce Buffett model\code\model3\combined_output_2025-05-13.csv';
+
+% Get the folder of the input file to save output in the same location
+[folderPath, ~, ~] = fileparts(filePath);
 
 % Use a datastore to handle large number of columns
 ds = tabularTextDatastore(filePath, ...
     'Delimiter', ',', ...
     'ReadVariableNames', false);  % Use false if you want to skip headers
-
-% Preview or check variable names
-% preview(ds)
 
 % Read all the data
 dataTable = readall(ds);
@@ -15,5 +15,10 @@ dataTable = readall(ds);
 % Convert to numeric array (if all columns are numeric)
 dataArray = table2array(dataTable);
 
-% Save the array to a .mat file
-save('combined_output.mat', 'dataArray');
+% Extract the first row as timesteps and the rest as gh_full
+timesteps = dataArray(1, :);
+gh_full = dataArray(2:end, :);
+
+% Save timesteps and gh_full to separate .mat files in the same folder
+save(fullfile(folderPath, 'timesteps.mat'), 'timesteps');
+save(fullfile(folderPath, 'gh_full.mat'), 'gh_full');
